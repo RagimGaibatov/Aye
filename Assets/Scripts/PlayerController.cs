d2025b4c4f635e7c7694e92a95f1d69808029f15
Ragim Gaibatov <ragimgaibatov@gmail.com>
2022-04-18 16:18:28 +0300

    initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private Animator playerAnim;
+    [SerializeField] private AudioSource playerAudio;
+    [SerializeField] private ParticleSystem explosionParticle;
+    [SerializeField] private ParticleSystem dirtParticle;
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] private float gravityModifier = 2;
+
+
+    [SerializeField] private AudioClip jumpSound;
+    [SerializeField] private AudioClip crashSound;
+
+    private float score = 0;
+
+    private float copyDashSpeed;
+    private float copyAnimSpeed;
+
+    public bool gameOver;
+
+    private int counterJump = 0;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        Physics.gravity *= gravityModifier;
+        copyDashSpeed = MoveLeft.dashSpeed;
+        copyAnimSpeed = playerAnim.speed;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOver && counterJump > 0)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+            counterJump--;
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            MoveLeft.dashSpeed = 1.5f;
+            playerAnim.speed = 2f;
+            score += 25 * Time.deltaTime;
+        }
+        else if (Input.GetKeyUp(KeyCode.R))
+        {
+            MoveLeft.dashSpeed = copyDashSpeed;
+            playerAnim.speed = copyAnimSpeed;
+            Debug.Log(score);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            counterJump = 2;
+            dirtParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            explosionParticle.Play();
+            playerAnim.SetInteger("DeathType_int", 1);
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+            dirtParticle.Stop();
+            Debug.Log(score);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] obstaclePrefab;
+
+    private Vector3 spawnPos = new Vector3(30, 0, 0);
+    private PlayerController playerControllerScript;
+    private int indexOfPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", 2f, 2f);
+    }
+
+
+    // Update is called once per frame
+    void SpawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            indexOfPrefab = Random.Range(0, obstaclePrefab.Length);
+            Instantiate(obstaclePrefab[indexOfPrefab], spawnPos, obstaclePrefab[indexOfPrefab].transform.rotation);
+        }
+    }
+}
